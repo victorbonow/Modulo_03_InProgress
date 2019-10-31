e5dab73bde6ba41ad9a0e9863c02f539505f149b
victorbonow <victorbonowx@gmail.com>
2019-10-30 23:25:31 -0300

    InProgress

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -15,7 +16,9 @@ class Database {
   init() {
     this.connetion = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connetion));
+    models
+      .map(model => model.init(this.connetion))
+      .map(model => model.associate && model.associate(this.connetion.models));
   }
 }
 
